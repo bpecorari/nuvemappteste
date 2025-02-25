b1271d89adc994e937182f36fbdc1a132f70ac9a
Bruno Loss <nuver@MacBook-Pro-de-Admin.local>
2025-02-25 08:37:04 -0300

    Corrige erro de timeout quando rodando localmente sem iframe

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,30 @@ import { useEffect, useState } from "react";
 import { connect, iAmReady } from "@tiendanube/nexo/helpers";
 import LowStockProducts from "./components/LowStockProducts";
 
-// Defina um tipo para a instância do Nexo. Se não tiver mais detalhes, use unknown.
-type NexoInstance = unknown;
-
 const App: React.FC = () => {
   const [isConnect, setIsConnect] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [nexoInstance, setNexoInstance] = useState<NexoInstance | null>(null);
+  const [isIframe, setIsIframe] = useState(false);
 
   useEffect(() => {
-    async function init() {
-      // Importa a função getNexo dinamicamente para garantir que seja executada no cliente
-      const { getNexo } = await import("../nexoClient");
-      const instance = getNexo();
-      if (!instance) return;
-      setNexoInstance(instance);
-      connect(instance).then(() => {
-        setIsConnect(true);
-        iAmReady(instance);
-      });
+    // Verifica se a aplicação está dentro de um iframe
+    if (window.self !== window.top) {
+      setIsIframe(true);
+      async function init() {
+        const { getNexo } = await import("../nexoClient");
+        const instance = getNexo();
+        if (!instance) return;
+        connect(instance).then(() => {
+          setIsConnect(true);
+          iAmReady(instance);
+        });
+      }
+      init();
+    } else {
+      console.warn("Rodando fora de um iframe. Nexo não será inicializado.");
     }
-    init();
   }, []);
 
-  if (!isConnect) return <div>Connecting...</div>;
+  if (isIframe && !isConnect) return <div>Connecting...</div>;
 
   return (
     <main>
