55affba0081259e8ae605a87000f2d794ed71db1
Bruno Loss <nuver@MacBook-Pro-de-Admin.local>
2025-02-25 08:25:38 -0300

    Corrigindo segundo commit

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            // Inclua o domínio ngrok temporariamente para testes
-            value: "frame-ancestors 'self' https://admin.nuvemshop.com.br https://*.nuvemshop.com.br https://7c54-2804-7f0-9602-a511-909b-b199-3adb-bb26.ngrok-free.app",
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://admin.nuvemshop.com.br https://*.nuvemshop.com.br",
           },
           {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Permitir a incorporação em iframe
           },
         ],
       },
